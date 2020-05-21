3f3fa2a628014e7ad8109351113798e9279810ca
vishuchhabra <vishuchhabra1016@gmail.com>
2020-05-21 21:59:41 +0530

    Some ready

---
+++ projbackend/app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const port =  4000
+
+app.listen(port,()=>
+{
+    console.log(`server is listening on port ${port}`)
+})
---
+++ projbackend/models/user.js
@@ -0,0 +1,82 @@
+const mongoose =  require('mongoose')
+const crypto =  require('crypto')
+const { v4: uuidv4 } = require('uuid');
+const userSchema =new  mongoose.Schema(
+{
+    name:{
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim :true //for removing the extra spaces trim is used
+    },
+    lastname:{
+        type:String,
+        maxlength : 32,
+        trim  : true
+    },
+    email: {
+        type: String,
+        trim : true,
+        required:true,
+        unique: true//for unique email  //property by mongoDB
+
+    },
+    userinfo:{
+        type:String,
+        trim: true
+    },
+    // password
+    encry_password:{
+        type: String,
+        required:true
+    },
+    salt : String,
+    role:{ //for the role
+        type:Number,
+        default:0
+    },
+    purchases:{  
+        type: Array,
+        default:[] //by default no  order
+    }
+
+})
+
+//virtual are for the setters and getters
+//using here virtual  we defined the new field in mongoose
+
+userSchema.virtual("password")
+  .set(function(password)
+  {
+        this._password = password //_ for private variable
+        this.salt  = uuidv4()
+        this.encry_password = this.securePassword(password)
+  })
+  .get(function()
+  {
+      return this._password;
+  })
+
+//method are basically operations on schema
+userSchema.methods = {
+    authenticate : function(plainpassword)
+    {
+        return this.securePassword(plainpassword) === this.encry_password //return true and false
+    },
+    securePassword: function(plainpassword)
+    {
+        //here we have used the virtual  field password
+        if(!plainpassword) return ""
+        try{
+            return crypto.createHmac('sha256', this.salt) //sha256 is used for cryptography
+                   .update(plainpassword)
+                   .digest('hex');
+        }
+        catch(err)
+        {
+            return ""
+        }
+    }
+} 
+
+module.exports = mongoose.model('User',userSchema)
