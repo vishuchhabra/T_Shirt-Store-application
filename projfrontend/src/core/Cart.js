d8078e5ca3288b996cfefd87e7234f4df6c94919
vishuchhabra <vishuchhabra1016@gmail.com>
2020-06-25 12:25:48 +0530

    Cart Completed

---
+++ projfrontend/src/core/Cart.js
@@ -0,0 +1,75 @@
+import React,{useEffect,useState} from "react";
+import "../styles.css";
+import { API } from "../backend";
+import Base from "./Base";
+import Card from "./card";
+import { loadCart } from "./helper/CartHelper";
+
+
+const  Cart = ()=> {
+
+    const [products ,setProducts] = useState([])
+
+    const [reload , setReload ] = useState(false)
+
+
+    
+    useEffect(()=>{
+       setProducts(loadCart())
+    },[reload ])
+    const loadAllProducts = () =>{
+        return(
+            <div>
+                <h2>Your Cart !! </h2>
+                
+                { products &&  products.map((product , index)=>{
+                    return(
+                        <div className="text-center mb-4">
+                    <Card
+                    key={index}
+                    product={product}
+                    addtoCart={false}
+                    removeFromCart={true}
+                    setReload={setReload}
+                    reload={reload}
+                    />
+                    </div>
+                    )
+
+                })}
+            </div>
+        )
+    }
+    const loadCheckout = () =>{
+        return(
+            <div>
+                <h2>This Seection for checkout </h2>
+            </div>
+        )
+    }
+
+
+
+
+
+    
+
+  return (
+    <Base title="Cart Page" description="Ready TO CheckOut">
+      <div className="row">
+          <div className="col-5" >
+            {loadAllProducts()}
+          </div>
+          <div className="col-2"></div>
+          <div className="col-5">
+
+              {loadCheckout()}
+              
+          </div>
+   
+      </div>
+    </Base>
+  );
+}
+
+export default Cart
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,23 +1,53 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
+import Card from "./card";
+import { getProducts } from "./helper/coreapicalls";
+
 
 export default function Home() {
-  console.log("API IS", API);
+
+  const [products ,setProducts] = useState([])
+  const [error ,setError]= useState(false)
+
+  const loadAllProduct = ()=>{
+    getProducts().then(
+      data =>{
+        if(data.error){
+          setError(data.error)
+
+        }
+        else{
+          setProducts(data)
+        }
+      }
+    )
+  }
+
+useEffect(()=>{
+     loadAllProduct()
+  },[])
+
+    
 
   return (
     <Base title="Home Page" description="Welcome To The T-shirt Store">
-      <div className="row">
-        <div className="col-4">
-          <button className="btn btn-success">TEST</button>
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">TEST</button>
-        </div>
-        <div className="col-4">
-          <button className="btn btn-success">TEST</button>
-        </div>
+      <div className="row text-center">
+        <div className="col-12 text-center">
+       <h1 className="text-white">
+        Hey , We Are Selling Here .....
+       </h1>
+       </div>
+       <div className="row">
+         {products.map((product, index)=>{
+           return(
+             <div key={index} className="col-lg-4 mb-4 mt-4">
+              <Card product={product}/>
+             </div>
+           )
+         })}
+       </div>
       </div>
     </Base>
   );
