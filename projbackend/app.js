9cd714213086ad6096a51e79de4910df6517c3b5
vishuchhabra <vishuchhabra1016@gmail.com>
2020-08-18 16:12:38 +0530

    Deployed Successfully

--- projbackend/app.js
+++ projbackend/app.js
@@ -47,6 +47,12 @@ app.use('/api',StripeRoutes)
 //PORT
 const port = process.env.PORT || 4000;
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 //Starting a server
 app.listen(port, () => {
   console.log(`server is running at ${port}`);
---
+++ projbackend/build/precache-manifest.02080a42fd462422722417cb8c5599ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc04cfda13da8612283bba04dd891a2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddb355ff1a8c2de8e8ef",
+    "url": "/static/css/main.d40ff29b.chunk.css"
+  },
+  {
+    "revision": "71e1ae39800070b02832",
+    "url": "/static/js/2.173c6667.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.173c6667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddb355ff1a8c2de8e8ef",
+    "url": "/static/js/main.d219c332.chunk.js"
+  },
+  {
+    "revision": "72017dee5443dfc32ffd",
+    "url": "/static/js/runtime-main.dc49cf19.js"
+  }
+]);
